962998ae0f5284a04913915d2f8c3ab5db776b5f
Angel Aguilar <angeljagsal@gmail.com>
2024-07-05 18:36:22 -0500

    Environment variables created (.env) for keys security

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ import neo4j from 'neo4j-driver'
 import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
-const neo4jUri = 'neo4j+s://9cb4d605.databases.neo4j.io';
-const neo4jUser = 'neo4j';
-const neo4jPass = 'Uxo5BB0g3-CTOU4n_w7wd-l_6gfoBwA4Uoe1VC9p7Q4';
+const neo4jUri = process.env.NEO4J_URI;
+const neo4jUser = process.env.NEO4J_USER;
+const neo4jPass = process.env.NEO4J_PASS;
 
 const driver = neo4j.driver(neo4jUri, neo4j.auth.basic(neo4jUser, neo4jPass));
 
@@ -18,13 +18,13 @@ const getSession = () => {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtS_HGCSbKjAUYNbGVBSuY5_qed7MuRk0",
-  authDomain: "travelappstorage.firebaseapp.com",
-  projectId: "travelappstorage",
-  storageBucket: "travelappstorage.appspot.com",
-  messagingSenderId: "828723833347",
-  appId: "1:828723833347:web:b88d5e2a4fcd42d2f1bc36",
-  measurementId: "G-FPKCKES69C"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
